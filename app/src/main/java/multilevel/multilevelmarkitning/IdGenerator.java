b6b3985dd507b5a60de38f639a6499fe2c670e3d
nvezones <nvezones@gmail.com>
2019-03-11 16:13:10 +0530

    now commit

--- app/src/main/java/multilevel/multilevelmarkitning/IdGenerator.java
+++ app/src/main/java/multilevel/multilevelmarkitning/IdGenerator.java
@@ -1,19 +1,21 @@
 package multilevel.multilevelmarkitning;
 
-import java.util.Random;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class IdGenerator {
 
     public static String generateId(String name)
     {
-//        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss");
-//        LocalDateTime now = LocalDateTime.now();
-//        String id=dtf.format(now).toString();
-//        id=id+name;
-        Random random=new Random();
-        int num=random.nextInt(2000);
-        Integer l=new Integer(num);
-        String id=l.toString();
+        String id=new String();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss");
+        LocalDateTime now = LocalDateTime.now();
+        String id=dtf.format(now).toString();
+        id=id+name;
+//        Random random=new Random();
+//        int num=random.nextInt(2000);
+//        Integer l=new Integer(num);
+//        String id=l.toString();
         return id;
     }
 
